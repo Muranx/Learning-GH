583fd3bcface6bf2976f0e176612bd28595a9af8
dmurans <dmurans@gmail.com>
2023-03-23 09:47:40 +0100

    Initial Commit

---
+++ M3L2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  M3L2
+//
+//  Created by Daniels Murans on 23.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3L2/M3L2App.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2App.swift
+//  M3L2
+//
+//  Created by Daniels Murans on 23.03.23.
+//
+
+import SwiftUI
+
+@main
+struct M3L2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
